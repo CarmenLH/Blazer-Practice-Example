6d0c2de05ea0ea98da906712ac4dac5827163999
Carmen <carmenlehanie@gmail.com>
2020-03-10 19:11:29 +0200

    Test run of Blazer

---
+++ BlazorDemoApp/BlazorDemoUI/Models/DisplayPersonModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoUI.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(3, ErrorMessage = "ID is too long.")]
+        [MinLength(1, ErrorMessage = "ID is too short.")]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "First Name is too long.")]
+        [MinLength(5, ErrorMessage = "First Name is too short.")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last Name is too long.")]
+        [MinLength(5, ErrorMessage = "Last Name is too short.")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
